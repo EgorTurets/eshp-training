7777f68c9a4c54d6798d8be476bff891e2959bc8
EgorTurets <EgorTurets@gmail.com>
2019-10-10 15:20:41 +0300

    add ServiceResult

---
+++ Eshp/src/Common/EshpCommon/ServiceResult.cs
@@ -0,0 +1,30 @@
+﻿namespace EshpCommon
+{
+    public class ServiceResult
+    {
+        public bool IsErrored { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public object Result { get; set; }
+
+        public ServiceResult() { }
+
+        private ServiceResult (object result, bool isErrored, string errorMessage)
+        {
+            IsErrored = isErrored;
+            ErrorMessage = errorMessage;
+            Result = result;
+        }
+
+        public static ServiceResult CreateErrorResult(bool isErrored, string errorMessage)
+        {
+            return new ServiceResult(null, isErrored, errorMessage);
+        }
+
+        public static ServiceResult CreateSuccessResult(object result)
+        {
+            return new ServiceResult(result, false, null);
+        }
+    }
+}
