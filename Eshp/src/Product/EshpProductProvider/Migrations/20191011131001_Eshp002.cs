00ba6be3ccaee7c3284c65826829ccdf2e8466b3
EgorTurets <EgorTurets@gmail.com>
2019-10-11 16:32:05 +0300

    add migration

---
+++ Eshp/src/Product/EshpProductProvider/Migrations/20191011131001_Eshp002.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EshpProductProvider.Migrations
+{
+    public partial class Eshp002 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    BaseDescription = table.Column<string>(nullable: true),
+                    BaseProductId = table.Column<int>(nullable: true),
+                    Price = table.Column<int>(nullable: true),
+                    Amount = table.Column<int>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    CompanyId = table.Column<int>(nullable: true),
+                    ProductId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductId",
+                table: "Products",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
