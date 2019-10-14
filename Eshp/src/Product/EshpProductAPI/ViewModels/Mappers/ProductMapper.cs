0c7d3b12a039f213c0c2e470fc6d449ea1fa7347
EgorTurets <EgorTurets@gmail.com>
2019-10-14 14:53:03 +0300

    add product mapper and company class

---
+++ Eshp/src/Product/EshpProductAPI/ViewModels/Mappers/ProductMapper.cs
@@ -0,0 +1,35 @@
+﻿using EshpProductAPI.ViewModels;
+using EshpProductCommon;
+
+namespace EshpProductService.Mappers
+{
+    public static class ProductMapper
+    {
+        public static ProductBase MapToModel (this ProductBaseVM vm)
+        {
+            if (vm == null)
+                return null;
+
+            return new ProductBase
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                BaseDescription = vm.BaseDescription,
+            };
+        }
+
+        public static ProductBaseVM MapToVM (this ProductBase model)
+        {
+            if (model == null)
+                return null;
+
+            return new ProductBaseVM
+            {
+                Id = model.Id,
+                Name = model.Name,
+                BaseDescription = model.BaseDescription,
+                OffersCount = model.Offers != null ? model.Offers.Count : 0,
+            };
+        }
+    }
+}
--- Eshp/src/UserCompany/UserService/Models/Company.cs
+++ Eshp/src/UserCompany/UserService/Models/Company.cs
@@ -1,6 +1,16 @@
-﻿namespace UserService
+﻿using EshpCommon;
+using EshpProductCommon;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace UserService
 {
-    public class Company
+    public class Company : NamedEntry
     {
+        public IdentityUser Contact { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public List<Product> Products { get; set; }
     }
 }
