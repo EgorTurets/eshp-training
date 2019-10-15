4ec2ebb40eeea29c5e9dd7f889b5aaff19f66911
EgorTurets <EgorTurets@gmail.com>
2019-10-15 10:01:49 +0300

    create user and company models

---
+++ EshpUserCompanyCommon/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿using EshpCommon;
+
+namespace EshpUserCompanyCommon.Models
+{
+    public class Company : NamedEntry
+    {
+        public string Address { get; set; }
+
+        public User Contact { get; set; }
+    }
+}
---
+++ EshpUserCompanyCommon/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EshpUserCompanyCommon.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        private string phoneNumber;
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                foreach (var symbol in value)
+                {
+                    if (Char.IsDigit(symbol))
+                    {
+                        throw new ArgumentException("Phone number can't have not numeric symbols.");
+                    }
+                }
+                
+                phoneNumber = value;
+            }
+        }
+
+    }
+}
