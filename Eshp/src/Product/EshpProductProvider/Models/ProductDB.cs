bf274c4c8c885f79e7280bd6ea66ff6a3e3d7627
EgorTurets <EgorTurets@gmail.com>
2020-02-06 15:36:25 +0300

    Add ProductDb model

---
+++ Eshp/src/Product/EshpProductProvider/Models/ProductDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EshpProductProvider.Models
+{
+    public class ProductDB
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? BaseProductId { get; set; }
+        public int? Price { get; set; }
+        public int? Amount { get; set; }
+        public int? CompanyId { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
